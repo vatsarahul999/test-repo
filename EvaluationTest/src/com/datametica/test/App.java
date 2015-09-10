d5a88586a3273159cb48edc293e0ff3e3b261e03
rahul vats <vatsa.rahul.999@gmail.com>
2015-09-10 10:40:02 +0530

    This is my answer

--- EvaluationTest/src/com/datametica/test/App.java
+++ EvaluationTest/src/com/datametica/test/App.java
@@ -1,12 +1,22 @@
 package com.datametica.test;
-class asd {
-	int a;
-	
-}
+
+import java.util.Scanner;
+
+
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Enter i:");
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		for (int i=0 ;i<n;i++){
+			for(int k=n-i-1;k>0;k--)
+				System.out.print("-");
+			for(int j=0;j<=i;j++)
+				System.out.print("*");
+			System.out.println();
+		}
 
 	}
 
